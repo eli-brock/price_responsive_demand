110c33b94e6b9a113643c0bfa00c250cd3e16371
Eli Brock <ebrock2k@gmail.com>
2023-10-05 19:52:21 +0000

    piecewise demand curve

--- simulate.py
+++ simulate.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cvxpy as cp
-from scipy.special import factorial
 
 def sim(
     L: list,
@@ -31,6 +30,7 @@ def sim(
     epsilon (N_L x T) -- price elasticity of demand
     price (N_L x T) -- the price associated with `demand`
     demand (N_L x T) -- the demand associated with `price`
+    # TODO: allow b as parameter
     b_total -- the total storage capacity (MWh)
     b_duration -- time for storage unit to fully charge/discharge (hours)
     p_line_max (N x N) -- thermal line limits (MW)
@@ -60,17 +60,25 @@ def sim(
     AL[L,:] = np.eye(len(L))
 
     # CVXPY variables
-    p_d = cp.Variable((len(L), T), name='p_d')
+    p_d_low = cp.Variable((len(L), T), name='p_d_low')
+    p_d_high = cp.Variable((len(L), T), name='p_d_high')
     p_g_int = cp.Variable((len(G), T), name='p_g_int')
     p_g_dis = cp.Variable((len(G),T), name='p_g_dis')
     p_b = cp.Variable((N,T), name='p_b')
     b = cp.Variable(N, name='b')
     b_0 = cp.Variable(N, name='b_0')
     delta = cp.Variable((N, T), name='delta')
+    z = cp.Variable((len(L),T), boolean=True)
+
+    # The maximum consumption at the value of lost load
+    p_voll = (voll/price)**epsilon*demand
+
+    # Numerical infinity
+    infinity = 1e2
 
     # CVXPY constraints
     constraints = [
-        AG@(p_g_dis+p_g_int)-AL@p_d-p_b == -B@delta,
+        AG@(p_g_dis+p_g_int)-AL@(p_d_low+p_d_high)-p_b == -B@delta,
         p_g_int <= p_g_int_max*generator_ratio,
         p_g_int >= 0,
         p_g_dis <= p_g_dis_max[:,np.newaxis]*(1-generator_ratio),
@@ -81,30 +89,42 @@ def sim(
         p_b >= -b[:,np.newaxis]/b_duration,
         cp.sum(b) == b_total,
         b_0 >= 0,
-        b_0 <= b
+        b_0 <= b,
+        p_d_low <= p_voll,
+        p_d_high >= 0,
+        p_d_low >= cp.multiply(p_voll,z),
+        p_d_high <= z*infinity
     ] + [
         cp.multiply(B, delta_t[:,np.newaxis]-delta_t[np.newaxis,:]) <= p_line_max
         for delta_t in delta.T
     ]
 
-    # The maximum consumption at the value of lost load
-    p_voll = (voll/price)**epsilon*demand
-
-    # Co-optimize over the supply- and demand-side cost functions. We use cost convention, so this is a minimization problem.
-    opf = cp.Problem(
-        cp.Minimize(
+    objective = cp.Minimize(
             cp.sum(
                 cost_lin@p_g_dis+cost_quad@cp.square(p_g_dis)
             ) - cp.sum(
-                voll*p_voll + cp.multiply(
+                voll*p_d_low + cp.multiply(
                     price/demand**(1/epsilon)*epsilon/(1+epsilon),
-                    cp.exp(cp.multiply((1/epsilon+1),cp.log(p_d))) - p_voll**(1/epsilon+1)
+                    cp.bmat(
+                        [
+                            [p_ij**((1/epsilon+1)[i,j]) for j, p_ij in enumerate(p_i)] 
+                            for i, p_i in enumerate(p_voll+p_d_high)
+                        ]
+                    ) - p_voll**(1/epsilon+1)
                 )
             )
-        ),
-        constraints
-    )
+        )
 
-    opf.solve(max_iters=10000)
+    # Co-optimize over the supply- and demand-side cost functions. We use cost convention, so this is a minimization problem.
+    cp.Problem(objective, constraints).solve()
+
+    # Re-solve using the optimal value of z as a parameter. This will give an equivalent result, but the SCIP solver used for the
+    # mixed-integer problem does not recover dual variables.
+    constraints[14:16] = [
+        p_d_low >= p_voll*z.value,
+        p_d_high <= z.value*infinity
+    ]
+    opf = cp.Problem(objective, constraints)
+    opf.solve()
 
     return opf
